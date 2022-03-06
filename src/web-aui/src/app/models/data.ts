e2f0830dd88881ac4ad61dd094f5738820e23366
Alex Watts <alexjwatts83@gmail.com>
2022-03-07 07:37:04 +1100

    wip adding models

--- src/web-aui/src/app/models/data.ts
+++ src/web-aui/src/app/models/data.ts
@@ -1,6 +1,11 @@
-export enum RegretTypes {
+export enum RegretType {
   foundation,
   boldness,
   moral,
   connection
+}
+
+export interface Regret {
+  regretType: RegretType,
+  name: string
 }
